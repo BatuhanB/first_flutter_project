7de7f894638f02f81bcf1925200b5241c3244f6a
BatuhanB <balibatuhan19@gmail.com>
2022-03-27 19:18:09 +0300

    Statesless Widget has been added to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-          backgroundColor: Colors.blueGrey[800],
-          appBar: AppBar(
-            title: (Center(
-              child: Text('Test Project'),
-            )),
-            backgroundColor: Colors.red[900],
-          ),
-          body: Center(
-            child: Image(
-              image: AssetImage('images/23.jpg'),
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        home: Scaffold(
+            backgroundColor: Colors.blueGrey[800],
+            appBar: AppBar(
+              title: (const Center(
+                child: Text('Test Project'),
+              )),
+              backgroundColor: Colors.red[900],
             ),
-          )),
-    ),
-  );
+            body: const Center(
+              child: Image(
+                image: AssetImage('images/23.jpg'),
+              ),
+            )),
+      );
 }
